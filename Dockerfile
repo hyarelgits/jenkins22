5e3876f6734e2b69e41fa729a96a58dbae3dfca4
hyarelgits <hyarel78@gmail.com>
2023-01-10 12:20:11 +0530

    fourth change1

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:8
-EXPOSE 8080
-ADD target/kiranhls.jar kiranhls.jar
-ENTRYPOINT ["java", "-jar", "/kiranhls.jarj
--- src/main/java/ss1.java
+++
@@ -1,7 +0,0 @@
-public class ss1 {
-
-    public static void main(String[] args) {
-        System.out.println("hyarel sonawane");
-    }
-}
-
